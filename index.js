7f2e8991fd27c6380080ded1e747b7d2ca8b4b1c
DrPersico <38354104+DrPersico@users.noreply.github.com>
2018-05-13 22:34:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,12 +10,17 @@ bot.on('message', msg => {
     if (msg.content === 'vem är bög?') {
         msg.channel.send('Marcus Svanström');
     }
+    if (msg.content === 'quien es mi amo?') {
+        msg.channel.send('<@208361671859765248>');
+    }
+
+    
 });
 
 bot.on("ready", () => {
-    bot.user.setActivity("with your mom");
+    bot.user.setActivity("con tu mami");
 });
-
+/*
 bot.on('message', msg => {
     if (msg.content === '3321') {
     var title = ['a', 'ga', 'gga', 'igga', 'Nigga'];
@@ -30,6 +35,22 @@ bot.on('message', msg => {
     }, 1000);  
 
 };
+
+});
+*/
+
+bot.on("ready", () => {
+    var title = ['a', 'ga', 'gga', 'igga', 'Nigga'];
+
+    var i = 0;  // the index of the current item to show
+
+
+    setInterval(function() {            // setInterval makes it run repeatedly
+        bot.channels.get("434008579838836740").setName(title[i++]);    // get the item and increment
+        if (i == title.length) i = 0;   // reset to first element if you've reached the end
+        bot.guilds.get("265583650165227520").roles.get("289055567480225793").setColor('#'+(Math.random()*0xFFFFFF<<0).toString(16));
+        bot.guilds.get("429774650961035264").roles.get("433029789612376074").setColor('#'+(Math.random()*0xFFFFFF<<0).toString(16));
+    }, 1000);   
 
 });
 
